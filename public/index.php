8dcbc05eb34954ab34fe34ab806a015da9850d73
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2020-01-09 23:54:45 +0000

    last try before sleep

--- public/index.php
+++ public/index.php
@@ -2,12 +2,12 @@
 /**
  * Website document root
  */
-define('DOCROOT', __DIR__.DIRECTORY_SEPARATOR);
-
-define('APPPATH', realpath(__DIR__.'/../app/').DIRECTORY_SEPARATOR);
-
-require APPPATH."bootstrap.php";
-
+//define('DOCROOT', __DIR__.DIRECTORY_SEPARATOR);
+//
+//define('APPPATH', realpath(__DIR__.'/../app/').DIRECTORY_SEPARATOR);
+//
+//require APPPATH."bootstrap.php";
+require "../vendor/autoload.php"
 $test = new App\Classes\TestController();
 echo $test->test();
 
