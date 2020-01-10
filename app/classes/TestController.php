85f1c1179f9b42d155e2008739789eb9cbe2b043
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2020-01-10 14:03:43 +0000

    merge everthign

---
+++ app/classes/TestController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\classes;
+
+
+class TestController
+{
+    public function test()
+    {
+        return "This is the test";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 <?php
 
-/** Just a test file */
 
-echo 'Checking mysql connection : ';
+define ('APPPATH', realpath(__DIR__.'/../app').DIRECTORY_SEPARATOR);
 
-new PDO('mysql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_NAME'), getenv('DB_USER'), getenv('DB_PASSWORD'));
-new mysqli(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASSWORD'), getenv('DB_NAME'));
+require APPPATH.'bootstrap.php';
 
-echo 'OK';
+$test = new \App\classes\TestController();
+
+echo $test->test();
