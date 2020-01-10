dc48ac6c90ae25a060c8ad2ec5ca48ad4628fc12
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2020-01-10 14:23:17 +0000

    solution found

--- public/index.php
+++ public/index.php
@@ -2,12 +2,13 @@
 /**
  * Website document root
  */
-//define('DOCROOT', __DIR__.DIRECTORY_SEPARATOR);
-//
-//define('APPPATH', realpath(__DIR__.'/../app/').DIRECTORY_SEPARATOR);
-//
-//require APPPATH."bootstrap.php";
-require "../vendor/autoload.php";
+define('DOCROOT', __DIR__.DIRECTORY_SEPARATOR);
+
+define('APPPATH', realpath(__DIR__.'/../app/').DIRECTORY_SEPARATOR);
+
+require APPPATH."bootstrap.php";
+//require "../vendor/autoload.php";
+
 $test = new App\Classes\TestController();
 echo $test->test();
 
